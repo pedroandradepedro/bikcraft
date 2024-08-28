b6f4c61d3a3b526f21fd6b0479a3b50c57ee4c4d
Pedro Andrade <pedrotrabalhos123@hotmail.com>
2024-08-28 00:57:41 -0300

    perguntas-seguro-js

--- js/script.js
+++ js/script.js
@@ -24,3 +24,23 @@ function ativarProduto(parametro) {
 }
 
 parametro.forEach(ativarProduto);
+
+// Perguntas Frequêntes
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(controls);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
